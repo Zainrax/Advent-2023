589f5d2c2b0616857495e886a2f3f4e7d40c1308
Patrick Baxter <patrickb321@gmail.com>
2023-12-02 23:32:58 +1300

    cleanup, add lowest_total_power
    Fairly simple addition, may be due to way I have planned it, or just a simple part 2. Main lesson learnt is to recheck the actual problem before commiting to a data structure refactor.

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::ops::AddAssign;
 
 enum Colour {
     Red,
@@ -40,12 +39,25 @@ impl Game {
         }
         true
     }
+
+    fn lowest_total_power(&self) -> u32 {
+        let value = self.totals.iter().fold(
+            CubeTotals::new(),
+            |mut ct, g| {
+                ct.red = ct.red.max(g.red);
+                ct.green = ct.green.max(g.green);
+                ct.blue = ct.blue.max(g.blue);
+                ct
+            },
+        );
+        value.red * value.blue * value.green
+    }
 }
 
 impl TryFrom<&str> for Game {
+    type Error = String;
+
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        //Example : Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-        dbg!(value);
         value
             .split_once(':')
             .and_then(|(game_str, totals_str)| {
@@ -76,7 +88,7 @@ impl TryFrom<&str> for Game {
                                     match colour {
                                         Colour::Red => c_t.red += num,
                                         Colour::Green => c_t.green += num,
-                                        Colour::Blue => c_t.blue += num,
+                                        Colour::Blue => c_t.blue += num
                                     };
                                     c_t
                                 })
@@ -88,11 +100,9 @@ impl TryFrom<&str> for Game {
             })
             .ok_or("Did not get the game".into())
     }
-
-    type Error = String;
 }
 
-fn process_input(max_cubes: CubeTotals, str_input: &String) -> u32 {
+fn process_input_one(max_cubes: CubeTotals, str_input: &str) -> u32 {
     let valid_games = str_input
         .lines()
         .flat_map(Game::try_from)
@@ -104,18 +114,34 @@ fn process_input(max_cubes: CubeTotals, str_input: &String) -> u32 {
     })
 }
 
+fn process_input_two(str_input: &str) -> u32 {
+    str_input
+        .lines()
+        .flat_map(Game::try_from)
+        .map(|g| g.lowest_total_power())
+        .sum()
+}
+
 fn main() {
-    fs::read_to_string("input.txt").map(|file| {
-        let res = process_input(
-            CubeTotals {
-                red: 12,
-                green: 13,
-                blue: 14,
-            },
-            &file,
-        );
-        println!("{}", res);
-    }).expect("No File Found");
+    fs::read_to_string("input.txt")
+        .map(|file| {
+            let res = process_input_one(
+                CubeTotals {
+                    red: 12,
+                    green: 13,
+                    blue: 14,
+                },
+                &file,
+            );
+            println!("{}", res);
+        })
+        .expect("No File Found");
+    fs::read_to_string("input.txt")
+        .map(|file| {
+            let res = process_input_two(&file);
+            println!("{}", res);
+        })
+        .expect("No File Found");
 }
 
 #[test]
@@ -128,14 +154,24 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     "#;
     assert_eq!(
-        process_input(
+        process_input_one(
             CubeTotals {
                 red: 12,
                 green: 13,
                 blue: 14
             },
-            &input.to_string()
+            input
         ),
         8
     );
+}
+
+#[test]
+fn test_2() {
+    let input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
+    assert_eq!(process_input_two(input), 2286);
 }
