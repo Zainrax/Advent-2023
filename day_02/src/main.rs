7fb43920b647c7f9cbbd962e0291a9721e846460
Patrick Baxter <patrickb321@gmail.com>
2023-12-02 20:03:30 +1300

    add day 2 test case

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,3 +1,19 @@
+fn process_input(str_input: &str) -> Option<u32> {
+    None
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_1() {
+    let input = r#"{
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    }"#;
+    assert_eq!(process_input(input), Some(8));
 }
