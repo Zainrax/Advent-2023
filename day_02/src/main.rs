8db74d6c06bf450eae7dbaa6bf7bf0a157ac14ad
Patrick Baxter <patrickb321@gmail.com>
2023-12-02 22:05:55 +1300

    add file input and get result
    Main focus was to try and provide some structure to implement traits where possible and keep in mind extensibility.

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::ops::AddAssign;
 
 enum Colour {
@@ -91,7 +92,7 @@ impl TryFrom<&str> for Game {
     type Error = String;
 }
 
-fn process_input(max_cubes: CubeTotals, str_input: &str) -> u32 {
+fn process_input(max_cubes: CubeTotals, str_input: &String) -> u32 {
     let valid_games = str_input
         .lines()
         .flat_map(Game::try_from)
@@ -104,21 +105,17 @@ fn process_input(max_cubes: CubeTotals, str_input: &str) -> u32 {
 }
 
 fn main() {
-    let input = r#"
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-    "#;
-    dbg!(process_input(
-        CubeTotals {
-            red: 12,
-            green: 13,
-            blue: 14,
-        },
-        input,
-    ));
+    fs::read_to_string("input.txt").map(|file| {
+        let res = process_input(
+            CubeTotals {
+                red: 12,
+                green: 13,
+                blue: 14,
+            },
+            &file,
+        );
+        println!("{}", res);
+    }).expect("No File Found");
 }
 
 #[test]
@@ -137,7 +134,7 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
                 green: 13,
                 blue: 14
             },
-            input
+            &input.to_string()
         ),
         8
     );
